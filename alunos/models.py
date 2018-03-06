45a716f912709e3f953b1a4c6455ea813254dc52
Laryssa Carvalho <laryssaalves_carvalho@hotmail.com>
2018-03-06 13:51:55 -0300

    Correção campo idade

--- alunos/models.py
+++ alunos/models.py
@@ -6,4 +6,4 @@ class Aluno(models.Model):
     nome = models.CharField('Nome', max_length=255)
     email = models.EmailField('Email',unique=True)
     ra = models.CharField('RA',max_length=20,null=False, unique=True)
-    idade = model.IntegerField('Idade')
+    idade = models.IntegerField('Idade')
