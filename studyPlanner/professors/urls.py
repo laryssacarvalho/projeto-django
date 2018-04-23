8e345e1d31bf23c780d61657ecd36f0fb4f06563
Daniel <dan.b.v.r05@gmail.com>
2018-04-22 21:58:56 -0300

    Back de turmas (prfessor)

--- studyPlanner/professors/urls.py
+++ studyPlanner/professors/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home'),    
+    path('home', views.home, name='home'),    
     path('meuPerfil', views.meuPerfil, name='meuPerfil'),
     path('agenda', views.agenda, name='agenda'),
     path('editar/<int:pk>/', views.ProfessorUpdate.as_view(), name='professor_update'),
     path('turmas', views.turmas, name='turmas'),
+    path('', views.sair, name='sair'),
+
+    path('turmas2', views.turmas2, name='turmas2'),
+    path('detalhe/<int:id>/', views.turmaDetail, name='detalhe'),
 
 ]
--- studyPlanner/professors/views.py
+++ studyPlanner/professors/views.py
@@ -1,32 +1,73 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from studyPlanner.core.models import Task
 from .forms import professorForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Professor
+from studyPlanner.core.models import Class
+from studyPlanner.core.models import User
+from studyPlanner.core.models import Task
 
 # Create your views here.
 
-class ProfessorUpdate(UpdateView):
-    model = Professor
-    fields = ['firstName','lastName','email','ra','tipo']
-
 def meuPerfil(request):
     if request.method == 'POST':
-        form = studentForm(request.POST)        
+        form = professorForm(request.POST)        
     else:
-        form = studentForm()
+        form = professorForm()
     return render(request, 'professors/perfil.html', {'nbar' : 'perfil', 'form': form})
 
 def agenda(request):
     return render(request, 'professors/agenda.html', {'nbar' : 'agenda'})
 
-def notas(request):
-    return render(request, 'professors/notas.html', {'nbar' : 'notas'})
-
 def home(request):
     return render(request, 'professors/home.html', {'nbar' : 'inicio'})
 
 def turmas(request):
-    return render(request, 'professors/turmas.html', {'nbar' : 'turmas'})
+    return render(request, 'professors/turmas.html', {'nbar' : 'inicio'})
+
+def sair(request):
+    return render(request, 'professors/home.html', {'nbar' : 'inicio'})
+
+class ProfessorUpdate(UpdateView):
+    model = Professor
+    fields = ['firstName','lastName','email','ra','tipo']
+
+# def turma_list(request):
+#     turmas = Class.objects()
+#     print(turmas)
+#     return render(request, 'professors/turmas.html', {'nbar' : 'turmas'})
+
+    
+def turmas2(request):
+    turmas = Class.objects.all()
+    #turmas = Class.objects.filter(idProfessor=profId)
+    context = {
+        'turmas': turmas
+    } 
+    return render(request, 'professors/turmas2.html', context)
+
+def turmaDetail(request, id):
+    turma = Class.objects.get(id = id)
+    alunos = turma.idStudent.all()
+    context = {
+        'alunos': alunos
+    } 
+    return render(request, 'professors/detalhe.html', context)
+
+
+def meuPerfil2(request):
+    me = Class.objects.all()
+    #turmas = Class.objects.filter(idProfessor=profId)
+    context = {
+        'turmas': turmas
+    } 
+    return render(request, 'professors/turmas2.html', context)
+
+def agenda2(request):
+    agenda = Class.objects.all()
+    #turmas = Class.objects.filter(idProfessor=profId)
+    context = {
+        'turmas': turmas
+    } 
+    return render(request, 'professors/turmas2.html', context)
