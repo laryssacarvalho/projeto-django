327579d3a2cc5d783d5f943f2302a2979a6d4d09
marikell <marikell_silva@hotmail.com>
2018-05-18 15:58:17 -0300

    new-changes

--- studyPlanner/core/static/js/login.js
+++ studyPlanner/core/static/js/login.js
@@ -17,13 +17,16 @@
     firebase.auth().signInWithPopup(provider).then(function(result) {
         var token = result.credential.accessToken;
         var user = result.user;
+        console.log("logou")
+       window.location.href = 'http://127.0.1:8000/alunos';
         
     }).catch(function(error) {
-        alert("Não logou!")
         var errorCode = error.code;
         var errorMessage = error.message;
         var email = error.email;
         var credential = error.credential;
+        console.log(errorMessage)
+
       });
 
   }
@@ -33,6 +36,9 @@
     firebase.auth().signInWithPopup(provider).then(function(result) {
         var token = result.credential.accessToken;
         var user = result.user;
+
+       window.location.href = 'http://127.0.1:8000/alunos';
+
       }).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -41,43 +47,43 @@
       });
   }
 
-$(function(){
-var textfield = $("input[name=user]");
-            $('button[type="submit"]').click(function(e) {
-                e.preventDefault();
-                //little validation just to check username
-                if (textfield.val() != "") {
-                    //$("body").scrollTo("#output");
-                    $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
-                    $("#output").removeClass(' alert-danger');
-                    $("input").css({
-                    "height":"0",
-                    "padding":"0",
-                    "margin":"0",
-                    "opacity":"0"
-                    });
-                    //change button text 
-                    $('button[type="submit"]').html("continue")
-                    .removeClass("btn-info")
-                    .addClass("btn-default").click(function(){
-                    $("input").css({
-                    "height":"auto",
-                    "padding":"10px",
-                    "opacity":"1"
-                    }).val("");
-                    });
+// $(function(){
+// var textfield = $("input[name=user]");
+//             $('button[type="submit"]').click(function(e) {
+//                 e.preventDefault();
+//                 //little validation just to check username
+//                 if (textfield.val() != "") {
+//                     //$("body").scrollTo("#output");
+//                     $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
+//                     $("#output").removeClass(' alert-danger');
+//                     $("input").css({
+//                     "height":"0",
+//                     "padding":"0",
+//                     "margin":"0",
+//                     "opacity":"0"
+//                     });
+//                     //change button text 
+//                     $('button[type="submit"]').html("continue")
+//                     .removeClass("btn-info")
+//                     .addClass("btn-default").click(function(){
+//                     $("input").css({
+//                     "height":"auto",
+//                     "padding":"10px",
+//                     "opacity":"1"
+//                     }).val("");
+//                     });
                     
-                    //show avatar
-                    /*$(".avatar").css({
-                        "background-image": "url('http://api.randomuser.me/0.3.2/portraits/women/35.jpg')"
-                    });*/
-                } else {
-                    //remove success mesage replaced with error message
-                    $("#output").removeClass(' alert alert-success');
-                    $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username ");
-                }
-                //console.log(textfield.val());
+//                     //show avatar
+//                     /*$(".avatar").css({
+//                         "background-image": "url('http://api.randomuser.me/0.3.2/portraits/women/35.jpg')"
+//                     });*/
+//                 } else {
+//                     //remove success mesage replaced with error message
+//                     $("#output").removeClass(' alert alert-success');
+//                     $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username ");
+//                 }
+//                 //console.log(textfield.val());
 
-            });
-});
+//             });
+// });
 
